14d9d3196e4d9ccd10e1b8ddad47f4be3a3b86f2
FrankieMFerreira <FrankieMFerreira@gmail.com>
2017-02-19 12:08:15 -0500

    Events can now be created with dates

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -5,5 +5,25 @@ class Api::V1::EventsController < ApplicationController
 		render json: @events
 	end
 
-end
+	def create
+		id = Auth.decode(response.request.env["HTTP_AUTHORIZATION"])[0]['user_id']
+		@event = Event.new(event_params)
+		@event.user_id = id
+		if @event.save
+			render json: @event
+		end
+	end
 
+		def show
+			@event = Event.find(params[:id])
+			render json: @event
+		end
+
+	end
+
+
+	private
+
+	def event_params
+		params.permit(:title, :description, :num_attendees, :party_template_id, :date)
+	end
