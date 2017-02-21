af05012cf7a4ebc1654c25f7df6aeda7af8b6bbb
Valerie McCarthy <valeriewilsonmccarthy@gmail.com>
2017-02-21 15:17:51 -0500

    party template item functionality added

--- app/serializers/api/v1/party_template_serializer.rb
+++ app/serializers/api/v1/party_template_serializer.rb
@@ -1,4 +1,5 @@
 class Api::V1::PartyTemplateSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :theme_category, :min_age, :max_age, :party_picture
   has_one :user
+  has_many :items
 end
