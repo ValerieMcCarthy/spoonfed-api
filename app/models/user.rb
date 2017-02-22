2d976da2c5975ea7bd71c63d23d3e918e4b72095
FrankieMFerreira <FrankieMFerreira@gmail.com>
2017-02-22 16:52:29 -0500

    user info

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
 	has_secure_password
 	has_many :events
 	has_many :party_templates
+	has_many :parties
 
-	
+
 
 
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :zipcode, :date_of_birth, :gender, :bio, :user_profile_picture, :party_templates, :events, :event_parties
+  attributes :id, :name, :email, :zipcode, :date_of_birth, :gender, :bio, :user_profile_picture, :party_templates, :parties
 
   # if yes to chnaging event relationship to user in user model:
   def event_parties
-  	
+
   	event_part = object.events.map do |temp|
   		temp.party_template
   	end
